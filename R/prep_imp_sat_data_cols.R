69f1fb2a1f691b9d40d625dcd179aa5ff1fbad21
Charles Rogers <charlesrogers@gmail.com>
2024-01-30 15:12:05 -0800

    debugging

--- R/prep_imp_sat_data_cols.R
+++ R/prep_imp_sat_data_cols.R
@@ -39,7 +39,7 @@ change_labeled_to_factors <- function(df){
 # Merging all the functions that are universal and putting them into a sub-function for th two functions we'll actually use
 prep_data <- function(df){
   # Switch column labels with variable labels
-  df <-  convert_labels_to_row_names(df)
+  df <- convert_labels_to_row_names(df)
   df <- remove_data_prefix(df)
   df <- remove_data_suffix(df)
   df <- replace_spaces_with_underscores(df)
@@ -47,25 +47,25 @@ prep_data <- function(df){
 }
 
 build_imp_column_names <- function(df, job_section) {
-  working_df <- prep_data(df)
+  df <- prep_data(df)
 # Add "imp__" to the beginning of the column name
 # Add "." + "job_section" variable to just after the "imp__" portion
-  new_col_name <- paste0("imp__", job_section, ".", colnames(working_df))
+  new_col_name <- paste0("imp__", job_section, ".", colnames(df))
   # Replace the original column name with the new one
-  working_df <- setNames(working_df, new_col_name)
+  df <- setNames(df, new_col_name)
 
-  return(working_df)
+  return(df)
 }
 
 build_sat_column_names <- function(df, job_section) {
-  working_df <- prep_data(df)
+  df <- prep_data(df)
   # Add "sat__" to the beginning of the column name
   # Add "." + "job_section" variable to just after the "sat__" portion
-  new_col_name <- paste0("sat__", job_section, ".", colnames(working_df))
+  new_col_name <- paste0("sat__", job_section, ".", colnames(df))
 
   # Replace the original column name with the new one
-  working_df <- setNames(working_df, new_col_name)
+  df <- setNames(df, new_col_name)
 
-  return(working_df)
+  return(df)
 }
 
